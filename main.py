1ece23afa6733887710917652c7377ef23886426
Gannonmcd <96160266+Gannonmcd@users.noreply.github.com>
2024-04-29 22:36:46 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+gatepin=11
+
+GPIO.setup(gatepin, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+
+def TurnOn():
+    GPIO.output(gatepin, 1)
+
+def TurnOff():
+    GPIO.output(gatepin, 0)
+
+def Flash(**kwargs):
+    flashes=kwargs.get(flashes, default=10)
+    time=kwargs.get(time, default=10)
+    endState=kwargs.get(endState, default=0)
+    for flash in range(flashes):
+        TurnOn()
+        time.sleep(time)
+        TurnOff()
+        time.sleep(time)
+        if(flash == flashes-1) and endState:
+            TurnOn()
