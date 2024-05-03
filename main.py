86947fefb7759b154f3c76f3459f25f48e82d46c
Gannon McDonald <gannonm@gmail.com>
2024-05-03 15:04:15 -0700

    corrected after first hardware run

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-import scheduler
+#import scheduler
 
 
 
@@ -10,24 +10,28 @@ GPIO.setwarnings(False)
 
 gatepin=11
 
-GPIO.setup(gatepin, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gatepin, GPIO.OUT)
 
 def TurnOn():
     GPIO.output(gatepin, 1)
+    print('turned on')
 
 def TurnOff():
     GPIO.output(gatepin, 0)
 
 def Flash(**kwargs):
-    flashes=kwargs.get(flashes, default=10)
-    time=kwargs.get(time, default=10)
-    endState=kwargs.get(endState, default=0)
+    flashes=kwargs.get('flashes', 10)
+    time_interval=kwargs.get('time_interval', .25)
+    endState=kwargs.get('endState', 1)
     for flash in range(flashes):
         TurnOn()
-        time.sleep(time)
+        time.sleep(time_interval)
         TurnOff()
-        time.sleep(time)
+        time.sleep(time_interval)
         if(flash == flashes-1) and endState:
             TurnOn()
 
-sys.argv[1]()
+if sys.argv[1]=='1': TurnOn()
+if sys.argv[1]=='2': TurnOff()
+if sys.argv[1]=='3': Flash()
+print('done')
